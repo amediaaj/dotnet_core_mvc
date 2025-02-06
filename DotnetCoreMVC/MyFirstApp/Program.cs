acf98d42ad25f552a4094022f24ef4eeeb71fbb8
Alex Amedia <aamedia@asu.edu>
2025-02-06 11:23:04 -0700

    Query String

--- DotnetCoreMVC/MyFirstApp/Program.cs
+++ DotnetCoreMVC/MyFirstApp/Program.cs
@@ -1,35 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-//app.MapGet("/", () => "Hello World!");
-
 app.Run(async (HttpContext context) => { 
     string path = context.Request.Path;
     string method = context.Request.Method;
 
-    //if(path == "/path1")...
-
-    context.Response.Headers["Content-Type"] = "text/html";
-    await context.Response.WriteAsync($"<h1>{path}</h1>");
-    await context.Response.WriteAsync($"<h1>{method}</h1>");
+    if (method == "GET")
+    {
+        context.Response.Headers["Content-Type"] = "text/html";
+        if (context.Request.Query.ContainsKey("id"))
+        {
+            string id = context.Request.Query["id"];
+            await context.Response.WriteAsync($"<p>{id}</p>");
+        }
+    }
 });
-
-//app.Run(async (HttpContext context) => {
-//    if (1 == 1)
-//    {
-//        context.Response.StatusCode = 200;
-//    }
-//    else
-//    {
-//        context.Response.StatusCode = 400;
-//    }
-//    context.Response.Headers["MyKey"] = "my value";
-//    context.Response.Headers["Content-Type"] = "text/html";
-//    await context.Response.WriteAsync("<h1>Hello</h1>");
-//    await context.Response.WriteAsync("<h2>World</h2>");
-
-//    //await context.Response.WriteAsync("Hello");
-//    //await context.Response.WriteAsync(" World");
-//});
 
 app.Run();
