c6203bc2c2f226f455912e616c27c11bf668dfbf
Alex Amedia <aamedia@asu.edu>
2025-02-06 11:35:58 -0700

    HTTP Request Headers

--- DotnetCoreMVC/MyFirstApp/Program.cs
+++ DotnetCoreMVC/MyFirstApp/Program.cs
@@ -5,14 +5,11 @@ app.Run(async (HttpContext context) => {
     string path = context.Request.Path;
     string method = context.Request.Method;
 
-    if (method == "GET")
+    context.Response.Headers["Content-Type"] = "text/html";
+    if (context.Request.Headers.ContainsKey("User-Agent"))
     {
-        context.Response.Headers["Content-Type"] = "text/html";
-        if (context.Request.Query.ContainsKey("id"))
-        {
-            string id = context.Request.Query["id"];
-            await context.Response.WriteAsync($"<p>{id}</p>");
-        }
+        string userAgent = context.Request.Headers["User-Agent"];
+        await context.Response.WriteAsync($"<p>{userAgent}</p>");
     }
 });
 
