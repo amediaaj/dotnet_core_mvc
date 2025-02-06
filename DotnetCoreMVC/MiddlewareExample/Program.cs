e71061f761bfb58c31383f7141004819995fe45b
Alex Amedia <aamedia@asu.edu>
2025-02-06 14:24:19 -0700

    Middleware Chain

--- DotnetCoreMVC/MiddlewareExample/Program.cs
+++ DotnetCoreMVC/MiddlewareExample/Program.cs
@@ -1,9 +1,30 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-// Short-circuiting middle-ware
-app.Run(async (HttpContext context) => {
+//middleware 1
+app.Use(async (HttpContext context, RequestDelegate next) => {
     await context.Response.WriteAsync("Hello");
+    // Forward the context to the next middleware
+    await next(context);
+    // More code
 });
+
+//middleware 2
+app.Use(async (HttpContext context, RequestDelegate next) => {
+    await context.Response.WriteAsync("Hello again");
+    // Forward the context to the next middlewar
+    // Or this is terminating middleware
+
+    //await next(context);
+    // More code
+});
+
+
+//middleware 3 w/Run i.e. terminating
+app.Run(async (HttpContext context) => {
+    await context.Response.WriteAsync("Hello again again");
+    // More code
+});
+
 
 app.Run();
