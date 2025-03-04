9adfe04671a6b165cf93af99b3f4d8edfd288a9b
Alex Amedia <aamedia@asu.edu>
2025-03-04 10:39:42 -0700

    Route constraints, Guid

--- DotnetCoreMVC/RoutingExample/Program.cs
+++ DotnetCoreMVC/RoutingExample/Program.cs
@@ -18,19 +18,19 @@ app.Use(async (context, next) => {
 // creating endpoints
 app.UseEndpoints(endpoints => {
     // URL must match files.{filename},{extension}
-    endpoints.Map("file/{filename}.{extension}", async (context) => {
+    endpoints.Map("files/{filename}.{extension}", async (context) => {
         string? fileName = Convert.ToString(context.Request.RouteValues["filename"]);
         string? extension = Convert.ToString(context.Request.RouteValues["extension"]);
 
-        await context.Response.WriteAsync($"In file: {fileName} - {extension}");
+        await context.Response.WriteAsync($"In files: {fileName} - {extension}");
     });
 
     // Default Parameter
     // "{parameter=dafault_value}"
     // Eg: employee/profile/defaultname
-    endpoints.Map("employee/profile/{employeename=defaultname}", async (context) => {
+    endpoints.Map("employees/profile/{employeename=defaultname}", async (context) => {
         string? employeeName = Convert.ToString(context.Request.RouteValues["employeename"]);
-        await context.Response.WriteAsync($"In employee profile: {employeeName}");
+        await context.Response.WriteAsync($"In employees profile: {employeeName}");
     });
 
     // Constraint
@@ -38,15 +38,15 @@ app.UseEndpoints(endpoints => {
     // Optional Route Parameter
     // "{parameter?}"
     // Eg: products/details/1
-    endpoints.Map("product/detail/{id:int?}", async (context) => {
+    endpoints.Map("products/detail/{id:int?}", async (context) => {
         // Will contain key id only if value for id was supplied
         if(context.Request.RouteValues.ContainsKey("id"))
         {
             int? id = Convert.ToInt32(context.Request.RouteValues["id"]);
-            await context.Response.WriteAsync($"In product detail: {id}");
+            await context.Response.WriteAsync($"In products detail: {id}");
         } else
         {
-            await context.Response.WriteAsync($"In product detail: id is not supplied");
+            await context.Response.WriteAsync($"In products detail: id is not supplied");
         }
     });
 
@@ -56,6 +56,15 @@ app.UseEndpoints(endpoints => {
     endpoints.Map("daily-digest-report/{reportdate:datetime}", async context => {
         DateTime reportDate = Convert.ToDateTime(context.Request.RouteValues["reportdate"]);
         await context.Response.WriteAsync($"In daily-digest-report: {reportDate.ToShortDateString()}");
+    });
+
+    // Eg: cities/cityid
+    endpoints.Map("cities/{cityid:guid}", async context => {
+        // Tools => Create GUID => 4. Registry Format
+
+        // ! indicates the value cannot be null
+        Guid cityId = Guid.Parse(Convert.ToString(context.Request.RouteValues["cityid"])!);
+        await context.Response.WriteAsync($"City information: {cityId}");
     });
 
 });
